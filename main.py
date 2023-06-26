00a315778648a715e1b4e131790b2cf63b0cbac3
bkitano <briankitano@gmail.com>
2023-06-26 12:10:38 -0400

    working kindergarten level compliance check

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from demo_chat_agent import agent_executor
 
 ## if main:
-if __name__ == '__main__':
-
-    agent_executor.run(input = "I want to buy a bottle of alcohol.", verbose=True)
+if __name__ == "__main__":
+    agent_executor.run(
+        input="I want to buy a bottle of soda for my friend Steve.", verbose=True
+    )
