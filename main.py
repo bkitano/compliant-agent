2740418266dc5e48165e4505a2e49a9aa012dcbc
bkitano <briankitano@gmail.com>
2023-06-26 12:14:48 -0400

    ha, added my old callback handler

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import os
 from demo_chat_agent import agent_executor
+from handler import AgentExecutorHandler
 
 ## if main:
 if __name__ == "__main__":
+    agent_executor_handler = AgentExecutorHandler("chat_session_id", "message_id")
+
     agent_executor.run(
-        input="I want to buy a bottle of soda for my friend Steve.", verbose=True
+        **{
+            "input": "I want to buy a bottle of soda for my friend Steve.",
+            "verbose": True,
+            "callbacks": [agent_executor_handler],
+        }
     )
