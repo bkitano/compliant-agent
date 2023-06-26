4e47b90193682dd3fd64f5c9f9944cd7638d4db6
bkitano <briankitano@gmail.com>
2023-06-26 11:59:03 -0400

    adding poetry-dotenv-plugin for reading envvars

--- demo_chat_agent.py
+++ demo_chat_agent.py
@@ -1,7 +1,7 @@
 from langchain.agents import ConversationalAgent, AgentExecutor
-from compliance_agent import compliance_tool
 from langchain.memory import ConversationBufferMemory
 from commons import chat_model
+from compliance_agent import mock_compliance_tool
 
 """
 This is a custom conversational chat agent. We are going to
@@ -14,7 +14,7 @@ prefix = """
 
 agent = ConversationalAgent.from_llm_and_tools(
     llm=chat_model,
-    tools=[compliance_tool],
+    tools=[mock_compliance_tool],
     prefix=prefix,
     input_variables=["input", "agent_scratchpad", "chat_history"],
 )
@@ -22,6 +22,6 @@ loaded_mem = ConversationBufferMemory(memory_key="chat_history")
 
 agent_executor = AgentExecutor.from_agent_and_tools(
     agent=agent,
-    tools=[compliance_tool],
+    tools=[mock_compliance_tool],
     memory=loaded_mem,
 )
