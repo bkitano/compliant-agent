408dfa88cfbe35a08497898cf1419de6fffecd75
bkitano <briankitano@gmail.com>
2023-06-26 12:03:24 -0400

    moving mock tool around

--- demo_chat_agent.py
+++ demo_chat_agent.py
@@ -1,7 +1,7 @@
 from langchain.agents import ConversationalAgent, AgentExecutor
 from langchain.memory import ConversationBufferMemory
 from commons import chat_model
-from compliance_agent import mock_compliance_tool
+from langchain.agents import Tool
 
 """
 This is a custom conversational chat agent. We are going to
@@ -12,11 +12,18 @@ it assesses the compliance of a prposed action.
 prefix = """
 """
 
+mock_compliance_tool = Tool(
+    name="Compliance Agent",
+    func=lambda x: "this is not compliant behavior",
+    description="Useful for when you need to check the legality of a specific action.",
+)
+
 agent = ConversationalAgent.from_llm_and_tools(
     llm=chat_model,
     tools=[mock_compliance_tool],
     prefix=prefix,
     input_variables=["input", "agent_scratchpad", "chat_history"],
+    verbose=True,
 )
 loaded_mem = ConversationBufferMemory(memory_key="chat_history")
 
@@ -24,4 +31,5 @@ agent_executor = AgentExecutor.from_agent_and_tools(
     agent=agent,
     tools=[mock_compliance_tool],
     memory=loaded_mem,
+    verbose=True,
 )
