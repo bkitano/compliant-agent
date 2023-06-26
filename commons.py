11ca67f2af4f3c16904997ea2afe496175d61a7d
bkitano <briankitano@gmail.com>
2023-06-26 09:53:43 -0400

    temperature=0

--- commons.py
+++ commons.py
@@ -3,8 +3,12 @@ from langchain.llms import Anthropic
 import os
 
 chat_model = ChatAnthropic(
-    model="claude-v1.3-100k", anthropic_api_key=os.environ["ANTHROPIC_API_KEY"]
+    temperature=0,
+    model="claude-v1.3-100k",
+    anthropic_api_key=os.environ["ANTHROPIC_API_KEY"],
 )
 llm_model = Anthropic(
-    model="claude-v1.3-100k", anthropic_api_key=os.environ["ANTHROPIC_API_KEY"]
+    temperature=0,
+    model="claude-v1.3-100k",
+    anthropic_api_key=os.environ["ANTHROPIC_API_KEY"],
 )
