693ee16d62463fbddc8b8290f7562d76101c39fc
bkitano <briankitano@gmail.com>
2023-06-26 15:32:40 -0400

    working conversation exit

--- demo_chat_agent.py
+++ demo_chat_agent.py
@@ -6,8 +6,25 @@ from langchain.agents import Tool
 """
 This is a custom conversational chat agent. We are going to
 modify the prefix and agent format instructions to ensure that 
-it assesses the compliance of a prposed action.
+it assesses the compliance of a proposed action.
 """
+
+PREFIX = """Assistant is a large language model trained by OpenAI.
+
+Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+
+Assistant is also responsible for ensuring that all of its actions comply with regulations. Assistant is capable of navigating the complexities of various industries and providing comprehensive guidance to users seeking compliance support.
+
+Assistant adopts a proactive approach in helping users by anticipating potential compliance challenges and offering preventive measures. In addition, if Assistant identifies that certain actions requested by the user may be non-compliant, Assistant will provide a warning and offer alternative solutions.
+
+TOOLS:
+------
+
+Assistant has access to the following tools:"""
 
 FORMAT_INSTRUCTIONS = """To use a tool, please use the following format:
 
@@ -28,10 +45,31 @@ Thought: Do I need to use a tool? No
 {ai_prefix}: [your response here]
 ```"""
 
+
+def toggle(item):
+    if "alcohol" in item:
+        return """<response>
+        <compliance_status>FALSE</compliance_status>
+        <reason>Alcohol is not allowed.</reason>
+    </response>"""
+    else:
+        return """<response>
+        <compliance_status>TRUE</compliance_status>
+    </response>"""
+
+
 mock_compliance_tool = Tool(
     name="Compliance Agent",
-    func=lambda x: f"this is compliant behavior: {x}",
-    description="Useful for when you need to check the legality of a specific action.",
+    func=toggle,
+    description="""Useful for when you need to check the legality or compliance of a specific action. The tool takes in a proposed action and returns whether or not the action is compliant.
+
+    The format of the output is:
+    <response>
+        <compliance_status>TRUE|FALSE</compliance_status>
+        <reason>REASON</reason>
+    </response>
+
+    If an action's compliance status is FALSE, it requires the agent to use the "Exit Tool", and pass the reason to the Exit Tool as to which rules the action is in violation of.""",
     verbose=True,
 )
 
@@ -40,11 +78,21 @@ mock_amazon_tool = Tool(
     func=lambda x: f"Successfully purchased a gift on Amazon: {x}",
     description="Useful for when you need to buy a gift on Amazon. The input should be the name of the gift. It will send out an order request for that gift to Amazon.",
     verbose=True,
+    return_direct=True,
+)
+
+exit_tool = Tool(
+    name="Exit Tool",
+    func=lambda x: f"Exiting...{x}",
+    description="Useful for when you need to exit the conversation. This tool will exit the conversation and return the reason for exiting.",
+    return_direct=True,
+    verbose=True,
 )
 
 agent = ConversationalAgent.from_llm_and_tools(
     llm=llm_model,
-    tools=[mock_compliance_tool, mock_amazon_tool],
+    tools=[mock_compliance_tool, mock_amazon_tool, exit_tool],
+    prefix=PREFIX,
     format_instructions=FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "chat_history"],
     verbose=True,
@@ -53,7 +101,7 @@ loaded_mem = ConversationBufferMemory(memory_key="chat_history")
 
 agent_executor = AgentExecutor.from_agent_and_tools(
     agent=agent,
-    tools=[mock_compliance_tool, mock_amazon_tool],
+    tools=[mock_compliance_tool, mock_amazon_tool, exit_tool],
     memory=loaded_mem,
     verbose=True,
 )
