cc42fa22d822b33f2588bb1ba5831e869fa7fc87
bkitano <briankitano@gmail.com>
2023-06-26 14:55:32 -0400

    removing extraneous arg error

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ if __name__ == "__main__":
     agent_executor.run(
         **{
             "input": "I want to buy a bottle of soda for my friend Steve.",
-            "verbose": True,
             "callbacks": [agent_executor_handler],
         }
     )
